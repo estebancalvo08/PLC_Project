fafaf780931d4f6b2e5851e826ff63042b103d87
Justin Thai <justithai@gmail.com>
2022-11-05 17:37:43 -0400

    added files and started

---
+++ src/main/java/plc/project/Analyzer.java
@@ -0,0 +1,122 @@
+package plc.project;
+
+import java.math.BigDecimal;
+import java.math.BigInteger;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.Optional;
+
+/**
+ * See the specification for information about what the different visit
+ * methods should do.
+ */
+public final class Analyzer implements Ast.Visitor<Void> {
+
+    public Scope scope;
+    private Ast.Function function;
+
+    public Analyzer(Scope parent) {
+        scope = new Scope(parent);
+        scope.defineFunction("print", "System.out.println", Arrays.asList(Environment.Type.ANY), Environment.Type.NIL, args -> Environment.NIL);
+    }
+
+    public Scope getScope() {
+        return scope;
+    }
+
+    @Override
+    public Void visit(Ast.Source ast) {
+        throw new UnsupportedOperationException();  // TODO
+    }
+
+    @Override
+    public Void visit(Ast.Global ast) {
+        throw new UnsupportedOperationException();  // TODO
+    }
+
+    @Override
+    public Void visit(Ast.Function ast) {
+        throw new UnsupportedOperationException();  // TODO
+    }
+
+    @Override
+    public Void visit(Ast.Statement.Expression ast) {
+        throw new UnsupportedOperationException();  // TODO
+    }
+
+    @Override
+    public Void visit(Ast.Statement.Declaration ast) {
+        Optional optional = ast.getValue();
+        if (optional.isPresent()) {
+            visit(ast.getValue().get());
+            scope.defineVariable(ast.getName(), ast.getName(), ast.getVariable().getType(), true, Environment.NIL);
+        }
+        return null;
+    }
+
+    @Override
+    public Void visit(Ast.Statement.Assignment ast) {
+        throw new UnsupportedOperationException();  // TODO
+    }
+
+    @Override
+    public Void visit(Ast.Statement.If ast) {
+        throw new UnsupportedOperationException();  // TODO
+    }
+
+    @Override
+    public Void visit(Ast.Statement.Switch ast) {
+        throw new UnsupportedOperationException();  // TODO
+    }
+
+    @Override
+    public Void visit(Ast.Statement.Case ast) {
+        throw new UnsupportedOperationException();  // TODO
+    }
+
+    @Override
+    public Void visit(Ast.Statement.While ast) {
+        throw new UnsupportedOperationException();  // TODO
+    }
+
+    @Override
+    public Void visit(Ast.Statement.Return ast) {
+        throw new UnsupportedOperationException();  // TODO
+    }
+
+    @Override
+    public Void visit(Ast.Expression.Literal ast) {
+        throw new UnsupportedOperationException();  // TODO
+    }
+
+    @Override
+    public Void visit(Ast.Expression.Group ast) {
+        throw new UnsupportedOperationException();  // TODO
+    }
+
+    @Override
+    public Void visit(Ast.Expression.Binary ast) {
+        throw new UnsupportedOperationException();  // TODO
+    }
+
+    @Override
+    public Void visit(Ast.Expression.Access ast) {
+        throw new UnsupportedOperationException();  // TODO
+    }
+
+    @Override
+    public Void visit(Ast.Expression.Function ast) {
+        throw new UnsupportedOperationException();  // TODO
+    }
+
+    @Override
+    public Void visit(Ast.Expression.PlcList ast) {
+        throw new UnsupportedOperationException();  // TODO
+    }
+
+    public static void requireAssignable(Environment.Type target, Environment.Type type) {
+        throw new UnsupportedOperationException();  // TODO
+    }
+
+}
